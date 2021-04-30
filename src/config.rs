750f80068d9fbef5abf42951a21196c0e3be77b8
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-04-30 18:21:34 +0100

    Fix variable name

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,13 @@
+use crate::camera_tokens::CameraToken;
+use crate::user_tokens::UserToken;
+use crate::CameraServerDbConn;
+use crate::{api_error::ApiError, users_cameras::check_if_user_has_access_to_camera};
+
 use super::schema::configs;
 use diesel::prelude::*;
 use diesel::{self};
+use rocket::http::Status;
+use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, AsChangeset, Insertable, Deserialize, Serialize)]
@@ -38,4 +45,52 @@ pub fn update(
 
 pub fn delete(camera_id: uuid::Uuid, connection: &PgConnection) -> QueryResult<usize> {
     diesel::delete(configs::table.find(camera_id)).execute(connection)
+}
+
+#[get("/Cameras/<camera_id_string>/GetConfigUser")]
+/// Retrieves a camera's config, authenticates with a user token.
+pub fn get_config_user(
+    conn: CameraServerDbConn,
+    camera_id_string: String,
+    user_token: UserToken,
+) -> Result<Json<Config>, ApiError> {
+    check_if_user_has_access_to_camera(&conn, &user_token, &camera_id_string)?;
+
+    let camera_id = uuid::Uuid::parse_str(&camera_id_string).map_err(|error| {
+        println!(
+            "Failed to parse camera id into UUID: Input was {}, error was {}",
+            camera_id_string, error
+        );
+        ApiError {
+            error: "Failed to parse camera ID string",
+            status: Status::UnprocessableEntity,
+        }
+    })?;
+
+    let config = get(camera_id, &conn).map_err(|error| {
+        println!("Failed to read camera config! The error was {}", error);
+        return ApiError {
+            error: "Failed to read config",
+            status: Status::InternalServerError,
+        };
+    })?;
+
+    Ok(Json(config))
+}
+
+#[get("/Cameras/GetConfigCamera")]
+/// Retrieves a camera's config, authenticates with a camera token.
+pub fn get_config_camera(
+    conn: CameraServerDbConn,
+    camera_token: CameraToken,
+) -> Result<Json<Config>, ApiError> {
+    let config = get(camera_token.camera_id, &conn).map_err(|error| {
+        println!("Failed to read camera config! The error was {}", error);
+        return ApiError {
+            error: "Failed to read config",
+            status: Status::InternalServerError,
+        };
+    })?;
+
+    Ok(Json(config))
 }
